79df066886135e80073252938cd2f199154e9db0
Hisamuddin Shaikh <hisamuddin94@gmail.com>
2025-01-11 02:15:57 +0530

    Added backend url in frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,39 +1,11 @@
-// import axios from 'axios';
-// import { GithubUser, Repository, UserDetails } from '../types';
-
-// const API_BASE_URL = 'http://localhost:3000/api';
-// const GITHUB_API = 'https://api.github.com';
-
-// const api = {
-//   getUser: async (username: string): Promise<UserDetails> => {
-//     const { data } = await axios.get(`${API_BASE_URL}/users/${username}`);
-//     return data;
-//   },
-
-//   getUserRepositories: async (username: string): Promise<Repository[]> => {
-//     const { data } = await axios.get(`${GITHUB_API}/users/${username}/repos`);
-//     return data;
-//   },
-
-//   searchUsers: async (query: string): Promise<UserDetails[]> => {
-//     const { data } = await axios.get(`${API_BASE_URL}/search?q=${query}`);
-//     return data;
-//   },
-
-//   getSortedUsers: async (sortBy: string): Promise<UserDetails[]> => {
-//     const { data } = await axios.get(`${API_BASE_URL}/users?sortBy=${sortBy}`);
-//     return data;
-//   }
-// };
-
-// export default api;
-
 
 import axios from 'axios';
 import { GithubUser, Repository, UserDetails } from '../types';
 
-const API_BASE_URL = 'http://localhost:3000/api';
-const GITHUB_API = 'https://api.github.com';
+const API_BASE_URL = process.env.REACT_APP_URL;
+const GITHUB_API = process.env.REACT_APP_GITHUB_API;
+// const API_BASE_URL = 'https://autonomize-assignment-jrvq.onrender.com/api';
+// const GITHUB_API = 'https://api.github.com';
 
 // Add error handling and type safety
 const api = {
